84e070a98d9761efa18a383a3e55849abc55c65c
kot8585 <ansgywjd321@gmail.com>
2023-01-31 23:27:03 +0900

    react-router outlet 사용해보기

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import NotFound from './pages/NotFound';
+import Root from './pages/Root';
+import Videos from './pages/Videos';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home/>},
+      { path: 'videos', element: <Videos/>},
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
